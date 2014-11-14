9e1b46c7a944b24bf88963fb31c7084a0b81410b
Jody Albritton <jody.albritton@gmail.com>
2014-11-13 23:09:46 -0600

    fix

--- lib/omniauth/strategies/wink.rb
+++ lib/omniauth/strategies/wink.rb
@@ -20,9 +20,9 @@ module OmniAuth
   module Strategies
     class Wink < OmniAuth::Strategies::OAuth2
       option :client_options, {
-        :site => 'https://winkapi.quirky.com',
-        :authorize_url => '/oauth2/authorize',
-        :token_url => '/oauth2/token'
+        :site => 'https://private-46e24-wink.apiary-mock.com',
+        :authorize_url => 'https://private-46e24-wink.apiary-mock.com',
+        :token_url => 'https://private-46e24-wink.apiary-mock.com/oauth2/token'
 
       }
 
